005842aa1fb5591aea1c77d76f3da029b25afbc1
jgomez89 <justin.gomez3789@gmail.com>
2025-07-30 19:05:37 -0700

    Commented out all majors that are BS+MS from main agent's tools

--- main_agent/agent.py
+++ main_agent/agent.py
@@ -6,21 +6,21 @@ from .sub_agents.CS import cs_agent
 from .sub_agents.ME import me_agent
 from .sub_agents.DS import ds_agent
 from .sub_agents.ONLINE import online_agent
-from .sub_agents.BEBM import bebm_agent
+# from .sub_agents.BEBM import bebm_agent
 from .sub_agents.BIEN import bien_agent
 from .sub_agents.CEN import cen_agent
-from .sub_agents.CHBM import chbm_agent
+# from .sub_agents.CHBM import chbm_agent
 from .sub_agents.CHEN import chen_agent
-from .sub_agents.CNBM import cnbm_agent
+# from .sub_agents.CNBM import cnbm_agent
 from .sub_agents.CSBA import csba_agent
-from .sub_agents.CSBM import csbm_agent
-from .sub_agents.EEBM import eebm_agent
+# from .sub_agents.CSBM import csbm_agent
+# from .sub_agents.EEBM import eebm_agent
 from .sub_agents.ELEN import elen_agent
-from .sub_agents.ENBM import enbm_agent
+# from .sub_agents.ENBM import enbm_agent
 from .sub_agents.ENEN import enen_agent
 from .sub_agents.ENRB import enrb_agent
 from .sub_agents.ENUN import enun_agent
-from .sub_agents.MCBM import mcbm_agent
+# from .sub_agents.MCBM import mcbm_agent
 from .sub_agents.MSE import mse_agent
 from .sub_agents.ENGR import engr_agent
 
@@ -39,6 +39,8 @@ schedule_recommender = LlmAgent(
     ),
     instruction=prompt.MAIN_PROMPT,
     output_key="schedule_recommender_output",
+
+    # Excluding BS + MS majors for now
     tools=[
         AgentTool(agent=cs_agent),
         AgentTool(agent=me_agent),
@@ -46,19 +48,19 @@ schedule_recommender = LlmAgent(
         AgentTool(agent=ds_agent),
         AgentTool(agent=bien_agent),
         AgentTool(agent=cen_agent),
-        AgentTool(agent=chbm_agent),
+        # AgentTool(agent=chbm_agent),
         AgentTool(agent=chen_agent),
-        AgentTool(agent=cnbm_agent),
-        AgentTool(agent=bebm_agent),
+        # AgentTool(agent=cnbm_agent),
+        # AgentTool(agent=bebm_agent),
         AgentTool(agent=csba_agent),
-        AgentTool(agent=csbm_agent),
-        AgentTool(agent=eebm_agent),
+        # AgentTool(agent=csbm_agent),
+        # AgentTool(agent=eebm_agent),
         AgentTool(agent=elen_agent),
-        AgentTool(agent=enbm_agent),
+        # AgentTool(agent=enbm_agent),
         AgentTool(agent=enen_agent),
         AgentTool(agent=enrb_agent),
         AgentTool(agent=enun_agent),
-        AgentTool(agent=mcbm_agent),
+        # AgentTool(agent=mcbm_agent),
         AgentTool(agent=mse_agent),
         AgentTool(agent=engr_agent)
     ]
